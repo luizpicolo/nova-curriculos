eb0eff318a922bb86ed017aa5030dcbc77eefff0
Luiz Picolo <luizpicolo@gmail.com>
2014-07-22 00:24:31 -0400

    Class ability cacan

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :access, :rails_admin
+      can :manage, :all   
+    else
+      can :read, :all
+    end
+  end
+end
