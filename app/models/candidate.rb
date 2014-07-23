843923291e28173d5786c0f4fadf951c3f0e6c0b
Luiz Picolo <luizpicolo@gmail.com>
2014-07-22 22:33:00 -0400

    Create entity candidate

---
+++ app/models/candidate.rb
@@ -0,0 +1,12 @@
+class Candidate < ActiveRecord::Base
+  validates :is_male, :inclusion => { :in => [true, false] }, presence: true
+  validates :bith_date, :mobile_phone, presence: true
+
+  has_one :user
+
+  has_many :professional_areas
+  has_many :hierarchical_levels
+  has_many :professional_experiences, dependent: :destroy
+
+  belongs_to :city
+end
