4c4b1da28c23471932ee9bb1e8cad9411b75f793
Luiz Picolo <luizpicolo@gmail.com>
2014-07-24 00:35:20 -0400

    Update file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,80 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722031632) do
+ActiveRecord::Schema.define(version: 20140724032613) do
+
+  create_table "academic_trainings", force: true do |t|
+    t.string   "schooling",      null: false
+    t.string   "name_of_course", null: false
+    t.date     "start_date",     null: false
+    t.date     "finish_date"
+    t.string   "institution",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "candidate_id"
+  end
+
+  create_table "candidates", force: true do |t|
+    t.boolean  "is_male",            null: false
+    t.date     "bith_date",          null: false
+    t.integer  "zip_code"
+    t.string   "street"
+    t.integer  "number"
+    t.string   "complement"
+    t.string   "home_fone"
+    t.string   "mobile_phone"
+    t.text     "courses_and_events"
+    t.integer  "city_id",            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "candidates", ["city_id"], name: "index_candidates_on_city_id", using: :btree
+
+  create_table "candidates_hierarchical_levels", force: true do |t|
+    t.integer  "candidate_id"
+    t.integer  "hierarchical_level_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "candidates_professional_areas", force: true do |t|
+    t.integer  "candidate_id"
+    t.integer  "professional_area_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cities", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hierarchical_levels", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "professional_areas", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "professional_experiences", force: true do |t|
+    t.string   "name_company",                 null: false
+    t.string   "last_post",                    null: false
+    t.date     "start_date",                   null: false
+    t.date     "finish_date"
+    t.boolean  "jobs_current", default: false, null: false
+    t.text     "attributions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "candidate_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -28,6 +101,7 @@ ActiveRecord::Schema.define(version: 20140722031632) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.boolean  "is_candidate",           default: true,  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
