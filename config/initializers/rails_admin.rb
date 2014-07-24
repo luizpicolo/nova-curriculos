2774d7013f75477035bf1403dffd686b2ba8e9f1
Luiz Picolo <luizpicolo@gmail.com>
2014-07-24 00:35:05 -0400

    Rails-Admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -30,7 +30,141 @@ RailsAdmin.config do |config|
     show_in_app
 
     ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+    history_index
+    history_show
+  end
+
+  ###  Academic Training  ###
+  config.model 'AcademicTraining' do
+    list do
+      field :id
+      field :candidate
+      field :schooling
+    end
+    edit do
+      field :schooling
+      field :name_of_course
+      field :start_date
+      field :finish_date
+      field :institution
+    end
+    # show do
+    #   field :id
+    #   field :candidate
+    #   field :schooling
+    # end
+  end
+
+  ###  Candidate  ###
+  config.model 'Candidate' do
+    list do
+      field :id
+      field :user
+      field :mobile_phone
+    end
+    edit do
+      configure :user, :belongs_to_association do
+        inline_add false
+        inline_edit false
+      end
+      configure :bith_date
+      configure :zip_code
+      configure :street
+      configure :number
+      configure :complement
+      configure :home_fone
+      configure :mobile_phone
+      configure :courses_and_events
+      configure :city
+      configure :is_male
+      configure :professional_areas
+      configure :hierarchical_levels
+      configure :professional_experiences
+      configure :academic_trainings
+    end
+    # show do
+    #   field :id
+    #   field :user
+    #   field :link
+    #   field :image
+    # end
+  end
+
+  ###  Hierarchical Level  ###
+  config.model 'HierarchicalLevel' do
+    list do
+      field :id
+      field :name
+    end
+    edit do
+      field :name
+    end
+    # show do
+    #   field :id
+    #   field :candidate
+    #   field :schooling
+    # end
+  end
+
+  ###  Professional area ###
+  config.model 'ProfessionalArea' do
+    list do
+      field :id
+      field :name
+    end
+    edit do
+      field :name
+    end
+    # show do
+    #   field :id
+    #   field :candidate
+    #   field :schooling
+    # end
+  end
+
+  ###  Professional Experience  ###
+  config.model 'ProfessionalExperience' do
+    list do
+      field :id
+      field :candidate
+      field :name_company
+      field :jobs_current
+    end
+    edit do
+      field :name_company
+      field :last_post
+      field :start_date
+      field :finish_date
+      field :jobs_current
+      field :attributions
+    end
+    # show do
+    #   field :name
+    #   field :email
+    #   field :admin
+    # end
+  end
+
+  ###  User  ###
+  config.model 'User' do
+    list do
+      field :id
+      field :name
+      field :email
+      field :updated_at
+      field :admin
+    end
+    edit do
+      field :name
+      field :email
+      field :password
+      field :password_confirmation
+      field :admin
+    end
+    show do
+      field :name
+      field :email
+      field :admin
+    end
   end
 end
