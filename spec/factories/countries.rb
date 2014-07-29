5a08d40e10bc0278dd4e9c25c33f562f581c9d0b
Luiz Picolo <luizpicolo@gmail.com>
2014-07-28 23:26:57 -0400

    Create Feature and test for entity Country

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,7 +1,5 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :country do
-    name "MyString"
+    name { Faker::Address.country }
   end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Country, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+		it 'should be invalid if there is no name' do
+			country = FactoryGirl.build :country, :name => nil
+			expect(country).to be_invalid
+		end
+
+		it "object created is valid" do
+			country = FactoryGirl.build(:country)
+			expect(country).to be_valid
+		end
+  end
+
+  describe "Associations" do
+  	it { should have_many(:states) }
+  end
 end
