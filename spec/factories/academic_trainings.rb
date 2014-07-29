4e1849418ff7529a5e9e1da9d55e1555b75aa8ce
Luiz Picolo <luizpicolo@gmail.com>
2014-07-28 23:25:18 -0400

    Create Feature and test for entity AcademicTraining

--- spec/factories/academic_trainings.rb
+++ spec/factories/academic_trainings.rb
@@ -1,11 +1,10 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :academic_training do
-    schooling "MyString"
-    name_of_course "MyString"
-    start_date "2014-07-22"
-    finish_date "2014-07-22"
-    institution "MyString"
+    schooling { Faker::Company.name }
+    name_of_course { Faker::Lorem.word }
+    start_date Time.now
+    finish_date Time.now
+    institution { Faker::Company.name }
+    candidate factory: :candidate
   end
 end
--- spec/models/academic_training_spec.rb
+++ spec/models/academic_training_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe AcademicTraining, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+		it 'should be invalid if there is no name schooling' do
+			academic_training = FactoryGirl.build :academic_training, :schooling => nil
+			expect(academic_training).to be_invalid
+		end
+
+		it 'should be invalid if there is no name of course' do
+			academic_training = FactoryGirl.build :academic_training, :name_of_course => nil
+			expect(academic_training).to be_invalid
+		end
+
+		it 'should be invalid if there is no start date' do
+			academic_training = FactoryGirl.build :academic_training, :start_date => nil
+			expect(academic_training).to be_invalid
+		end
+
+		it 'should be invalid if there is no name institution' do
+			academic_training = FactoryGirl.build :academic_training, :institution => nil
+			expect(academic_training).to be_invalid
+		end
+
+		it "object created is valid" do
+			academic_training = FactoryGirl.build(:academic_training)
+			expect(academic_training).to be_valid
+		end
+  end
+
+  describe "Association" do
+  	it { should belong_to(:candidate) }
+  end
 end
