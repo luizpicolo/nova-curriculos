a80c3069dcb1038eed899407e39afe0eb918d6a2
Luiz Picolo <luizpicolo@gmail.com>
2014-07-29 22:29:58 -0400

    Create validations for entity Extra Course

--- spec/models/extra_course_spec.rb
+++ spec/models/extra_course_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ExtraCourse, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+		it 'should be invalid if there is no fancy name' do
+			extra_course = FactoryGirl.build :extra_course, :name => nil
+			expect(extra_course).to be_invalid
+		end
+
+		it 'should be invalid if there is no local name' do
+			extra_course = FactoryGirl.build :extra_course, :local => nil
+			expect(extra_course).to be_invalid
+		end
+
+		it 'should be invalid if there is no description' do
+			extra_course = FactoryGirl.build :extra_course, :description => nil
+			expect(extra_course).to be_invalid
+		end
+
+		it "object created is valid" do
+			extra_course = FactoryGirl.build(:extra_course)
+			expect(extra_course).to be_valid
+		end
+  end
+
+  describe "Associations" do
+  	it { should belong_to(:candidate) }
+  end
 end
