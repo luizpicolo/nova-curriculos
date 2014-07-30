5923508dd59d86f6869a9f0b7a0452462219fedf
Luiz Picolo <luizpicolo@gmail.com>
2014-07-29 22:30:29 -0400

    Create validations for entity hierarchical Level

--- spec/models/hierarchical_leval_spec.rb
+++ spec/models/hierarchical_leval_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe HierarchicalLeval, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe HierarchicalLevel, :type => :model do
+  describe "Validations" do
+		it 'should be invalid if there is no name' do
+			hierarchical_leval = FactoryGirl.build :hierarchical_level, :name => nil
+			expect(hierarchical_leval).to be_invalid
+		end
+
+		it "object created is valid" do
+			hierarchical_leval = FactoryGirl.build(:hierarchical_level)
+			expect(hierarchical_leval).to be_valid
+		end
+  end
+
+  describe "Associations" do
+  	it { should have_and_belong_to_many(:candidates) }
+  end
 end
