82171a273760645551a8ecc505c99e2c2bfa46b9
Luiz Picolo <luizpicolo@gmail.com>
2014-07-30 22:28:42 -0400

    initial implementation layout

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,22 @@
+// Close Alerts
+$(document).ready(function() {
+	$(".close").click(function(){
+	    $(this).parent().fadeOut(500);
+	})
+	$(".alert").click(function(){
+	    $(this).fadeOut(500);
+	})
+});
+
+$(document).ready(function(){
+	active();
+	// first_last('elements');
+	// even_odd('elements');
+	// page_up();
+	// page_back();
+});
+
+$(function(){
+	$('.scroll').jScrollPane();
+});
+
