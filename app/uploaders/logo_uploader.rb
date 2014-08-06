d9aa5054f4298d32a09e3923fabe1cd950a8ecc7
Luiz Picolo <luizpicolo@gmail.com>
2014-08-05 23:29:35 -0400

    path upload (Test)

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -4,7 +4,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,7 +13,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/home/production/railsapp/storage/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
