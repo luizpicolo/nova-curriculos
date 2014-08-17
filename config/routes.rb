57b80a652bdf8059198cfa60eced57eefe1b67d8
Luiz Picolo <luizpicolo@gmail.com>
2014-08-16 20:42:32 -0400

    Added routes for academic training

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resource :candidates
-  resource :academic_trainings
 
   # Routes
   get "/oportunidades/:slug" => "jobs#show", :as => :opportunities
@@ -26,6 +25,14 @@ Rails.application.routes.draw do
   get "/candidato/curriculo" => "candidates#show", :as => :candidate
   post "/candidato/curriculo" => "candidates#create"
   patch "/candidato/curriculo" => "candidates#update"
+
+  # Academic Trainings
+  get "/candidato/curriculo/fa/:academic_training_id" => "candidates#show", :as => :edit_academic_trainings
+  get "/academic_trainings" => "academic_trainings#index", :as => :academic_trainings
+  get "/academic_trainings" => "academic_trainings#index", :as => :academic_training
+  post "/academic_trainings" => "academic_trainings#create", :as => :create_academic_trainings
+  patch "/academic_trainings" => "academic_trainings#update", :as => :update_academic_trainings
+  delete "/academic_trainings/:id" => "academic_trainings#destroy", :as => :destroy_academic_trainings
 
   # Route Root
   root 'home#index'
