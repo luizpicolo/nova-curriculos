8e8ea54b8e682a9d01ab993c681416c85164d65b
Luiz Picolo <luizpicolo@gmail.com>
2014-09-08 16:35:04 -0400

    [skip ci] Fixed

--- app/mailers/welcome_message_candidate.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeMailer < ActionMailer::Base
-  default from: "noreply@novacurriculos.com.br"
-
-  def welcome_message_candidate(contact)
-    @contact = contact
-    mail(:to => @contact.email, :subject => 'Mensagem de Boas Vindas')
-  end
-end
