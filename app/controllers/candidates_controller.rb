5efe8a4125def258c0354dead28716b7f2508793
Luiz Picolo <luizpicolo@gmail.com>
2014-09-29 01:15:24 -0400

    [skip ci] Fixed

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -36,7 +36,7 @@ class CandidatesController < ApplicationController
     if !candidate.nil?
       @candidate = candidate
     else
-      redirect_to request.referer, :flash => { :error => "Este candidado não atualizou seu currículo. Por favor, atualize e pague com um post = )" }
+      redirect_to show_curriculum_candidate_path, :flash => { :error => "Este candidado não atualizou seu currículo. Por favor, atualize e pague com um post = )" }
     end
   end
 
